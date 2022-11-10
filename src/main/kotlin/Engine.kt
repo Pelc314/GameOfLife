f4dc233d0e762835a34aabefb5f3fec011a55526
Pelc314 <mpepelc@gmail.com>
2022-11-10 20:45:34 +0100

    Created every relevant test for testing basic rules of the Game Of Life

--- src/main/kotlin/Engine.kt
+++ src/main/kotlin/Engine.kt
@@ -1,6 +1,6 @@
-
-class Engine(val cell: Cell) {
-    fun stateChanger(neihbourCount:Int){
-        cell.isAlive = true
+class Engine(private val cell: Cell) {
+    fun stateChanger(neighbourCount: Int) {
+        if (cell.isAlive) this.cell.isAlive = (neighbourCount == 2 || neighbourCount == 3)
+        else this.cell.isAlive = (neighbourCount == 3)
     }
 }
--- src/test/kotlin/EngineTest.kt
+++ src/test/kotlin/EngineTest.kt
@@ -2,12 +2,103 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.Test
 
 internal class EngineTest {
-    private val cell = Cell(false)
-    private val engineTest = Engine(cell)
-
     @Test
     fun livingCellWithZeroNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
         engineTest.stateChanger(0)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithOneNeighbourDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(1)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithTwoNeighboursSurvives() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(2)
+        assertEquals(true,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithThreeNeighboursSurvives() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(3)
+        assertEquals(true,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithFourNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(4)
+        assertEquals(false,cell.isAlive)
+    }
+
+    @Test
+    fun livingCellWithFiveNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(5)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithSixNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(6)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithSevenNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(7)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun livingCellWithEightNeighboursDies() {
+        val cell = Cell(true)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(8)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun deadCellWithThreeNeighboursIsRevived() {
+        val cell = Cell(false)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(3)
+        assertEquals(true,cell.isAlive)
+    }
+    @Test
+    fun deadCellWithZeroNeighboursStaysDead() {
+        val cell = Cell(false)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(0)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun deadCellWithOneNeighbourStaysDead() {
+        val cell = Cell(false)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(1)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun deadCellWithTwoNeighbourStaysDead() {
+        val cell = Cell(false)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(2)
+        assertEquals(false,cell.isAlive)
+    }
+    @Test
+    fun deadCellWithFourNeighbourStaysDead() {
+        val cell = Cell(false)
+        val engineTest = Engine(cell)
+        engineTest.stateChanger(4)
         assertEquals(false,cell.isAlive)
     }
 }
