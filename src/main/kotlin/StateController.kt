fa643363e67f17d10dac6c625492dfc176b569fc
Pelc314 <mpepelc@gmail.com>
2022-11-10 21:38:16 +0100

    Refactored tests and Refactored Engine Class to StateController

--- src/main/kotlin/StateController.kt
+++ src/main/kotlin/StateController.kt
@@ -1,4 +1,4 @@
-class Engine(private val cell: Cell) {
+class StateController(private val cell: Cell) {
     fun stateChanger(neighbourCount: Int) {
         if (cell.isAlive) this.cell.isAlive = (neighbourCount == 2 || neighbourCount == 3)
         else this.cell.isAlive = (neighbourCount == 3)
---
+++ src/test/kotlin/StateControllerTest.kt
@@ -0,0 +1,71 @@
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
+internal class StateControllerTest {
+    @Test
+    fun livingCellWithLessThanTwoNeighboursDies() {
+        val cell = Cell(true)
+        val stateControllerTest = StateController(cell)
+        var tmp = 0
+        while (tmp < 2) {
+            stateControllerTest.stateChanger(tmp)
+            assertEquals(false, cell.isAlive)
+            tmp++
+        }
+    }
+
+    @Test
+    fun livingCellWithTwoOrThreeNeighboursSurvives() {
+        val cell = Cell(true)
+        val stateControllerTest = StateController(cell)
+        var tmp = 2
+        while (tmp < 4) {
+            stateControllerTest.stateChanger(tmp)
+            assertEquals(true, cell.isAlive)
+            tmp++
+        }
+    }
+    @Test
+    fun livingCellWithFourOrMoreNeighboursDies() {
+        val cell = Cell(true)
+        val stateControllerTest = StateController(cell)
+        var tmp = 4
+        while (tmp < 9) {
+            stateControllerTest.stateChanger(tmp)
+            assertEquals(false, cell.isAlive)
+            tmp++
+        }
+    }
+
+    @Test
+    fun deadCellWithThreeNeighboursIsRevived() {
+        val cell = Cell(false)
+        val stateControllerTest = StateController(cell)
+        stateControllerTest.stateChanger(3)
+        assertEquals(true, cell.isAlive)
+    }
+
+    @Test
+    fun deadCellWithLessThanThreeNeighboursStaysDead() {
+        val cell = Cell(false)
+        val stateControllerTest = StateController(cell)
+        var tmp = 0
+        while (tmp < 3) {
+            stateControllerTest.stateChanger(tmp)
+            assertEquals(false, cell.isAlive)
+            tmp++
+        }
+    }
+
+    @Test
+    fun deadCellWithFourOrMoreNeighbourStaysDead() {
+        val cell = Cell(false)
+        val stateControllerTest = StateController(cell)
+        var tmp = 4
+        while (tmp < 9) {
+            stateControllerTest.stateChanger(tmp)
+            assertEquals(false, cell.isAlive)
+            tmp++
+        }
+    }
+}
